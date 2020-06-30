37f2932085b56eab1e0845777bf515c487d97c88
charllab <developer.charl@gmail.com>
2020-06-30 12:41:43 -0700

    launch updates

--- src/themes/divi-child/footer.php
+++ src/themes/divi-child/footer.php
@@ -2,8 +2,8 @@
         <div class="container text-primary">
             <div class="row">
                 <div class="col-lg-6 text-center text-lg-left">
-                    <p class="mb-0">&copy; <?php echo Date('Y') . ' ' . get_bloginfo('name'); ?></p>
-                    <p class="mb-75 mb-0">Join us on <a href="#" target="_blank" class="text-primary font-weight-bold">Facebook</a> | <a
+                    <p class="mb-75 mb-0">&copy; <?php echo Date('Y') . ' ' . get_bloginfo('name'); ?></p>
+                    <p class="d-none mb-75 mb-0">Join us on <a href="#" target="_blank" class="text-primary font-weight-bold">Facebook</a> | <a
                             href="#" target="_blank" class="text-primary font-weight-bold">Instagram</a></p>
                 </div>
                 <div class="col-lg-6 text-center text-lg-right">
--- src/themes/divi-child/header.php
+++ src/themes/divi-child/header.php
@@ -26,6 +26,27 @@
         document.documentElement.className = 'js';
     </script>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-4700177-76"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-4700177-76');
+    </script>
+    <!-- Hotjar Tracking Code for https:makingchangehappen.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1881004,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
